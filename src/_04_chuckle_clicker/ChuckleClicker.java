78273062f2c7d973c39afda1824ee374d6ed57c6
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2019-09-25 16:55:05 -0700

    ChuckleClicker PigLatinGenerator

--- src/_04_chuckle_clicker/ChuckleClicker.java
+++ src/_04_chuckle_clicker/ChuckleClicker.java
@@ -1,22 +1,27 @@
 package _04_chuckle_clicker;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class ChuckleClicker {
+public class ChuckleClicker implements ActionListener {
 	
 public static void main(String[] args) {
-	makeButtons();
+	ChuckleClicker chuckle = new ChuckleClicker();
+	chuckle.makeButtons();
 }
-static void makeButtons() {
-	JFrame frame = new JFrame();
-	JPanel panel = new JPanel();
-	JButton button = new JButton();
-	JButton button1 = new JButton();
-	frame.add(panel);
+JFrame frame = new JFrame();
+JPanel panel = new JPanel();
+JButton button = new JButton();
+JButton button1 = new JButton();
+
+void makeButtons() {
+		frame.add(panel);
 	panel.add(button);
 	panel.add(button1);
 	button.setText("Joke");
@@ -24,5 +29,19 @@ static void makeButtons() {
 	button.setPreferredSize(new Dimension(200,100));
 	button1.setPreferredSize(new Dimension(200,100));
     frame.setVisible(true);
+    button.addActionListener(this);
+    button1.addActionListener(this);
+}
+@Override
+public void actionPerformed(ActionEvent e) {
+	// TODO Auto-generated method stub
+	JButton buttonPressed = (JButton) e.getSource();
+	if (buttonPressed == button){
+		JOptionPane.showMessageDialog(null, "Joe");
+	}
+	 if (buttonPressed == button1) {
+		JOptionPane.showMessageDialog(null, "Mama");
+	}
+	
 }
 }
